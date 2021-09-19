9c9e9f001e639ee9ab47790bba27570b1652a31e
Dave Cooper <dcooper@uab.edu>
2021-09-19 06:55:52 -0500

    Updated homepage with ethan's image

--- ThermoPropertyCalculator/views.py
+++ ThermoPropertyCalculator/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def ThermoPropertyCalculator(request):
-  return render(request,'ThermoPropertyCalculator/index.html')
+  return render(request,'ThermoPropertyCalculator/ThermoPropertyCalculator.html')
