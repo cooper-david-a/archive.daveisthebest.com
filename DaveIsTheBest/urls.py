8dae64f5b8b95db9937b6f6de157b72ea4183297
Dave Cooper <dcooper@uab.edu>
2021-10-10 10:46:09 -0500

    Installed debug toolbar

--- DaveIsTheBest/urls.py
+++ DaveIsTheBest/urls.py
@@ -13,13 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('__debug__/',include(debug_toolbar.urls)),
     path('', views.home,name='home'),
     path('HIIT_Timer/', include('HIIT_Timer.urls')),
     path('ThermoPropertyCalculator/', include('ThermoPropertyCalculator.urls')),
+    path('todo/', include('todo.urls')),
 ]
---
+++ todo/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+  path('', views.TodoView.as_view(), name='todo')
+]
