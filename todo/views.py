081f40d884b69baa543df3830e80b1647795f08a
Dave Cooper <dcooper@uab.edu>
2021-10-31 10:19:19 -0500

    setup todo list with its own template.

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import TodoItem
+from .models import TodoItem, TodoList
 
 
 class TodoDetailView(generic.DetailView):
   model = TodoItem
   template_name = 'todo/detail.html'
+
+class TodoListView(generic.ListView):
+  model = TodoList
+  template_name = 'todo/list.html'
