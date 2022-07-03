8046045594cc89384c60fa6d5db6f9407b5fa7cd
Dave <cooper.david.a@protonmail.com>
2022-07-03 16:45:55 -0500

    try again

--- DaveIsTheBest_base/apps.py
+++ DaveIsTheBest_base/apps.py
@@ -6,7 +6,7 @@ class DaveisthebestBaseConfig(AppConfig):
     BASE_DIR = settings.BASE_DIR
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'DaveIsTheBest_base'
-    with open(BASE_DIR / '/DaveIsTheBest_base/spam_filter_vectorizer.sav','rb') as sv:
+    with open(BASE_DIR / 'DaveIsTheBest_base/spam_filter_vectorizer.sav','rb') as sv:
         spam_vectorizer = load(sv)
-    with open(BASE_DIR / '/DaveIsTheBest_base/spam_filter_model.sav','rb') as sf:
+    with open(BASE_DIR / 'DaveIsTheBest_base/spam_filter_model.sav','rb') as sf:
         spam_filter_model = load(sf)
