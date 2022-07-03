fd9feb895f8ecd0016a66075030fd4a13cad7bcb
Dave <cooper.david.a@protonmail.com>
2022-07-03 16:39:54 -0500

    Delt with path issues

--- DaveIsTheBest_base/apps.py
+++ DaveIsTheBest_base/apps.py
@@ -1,10 +1,12 @@
 from django.apps import AppConfig
 from pickle import load
+from DaveIsTheBest import settings
 
 class DaveisthebestBaseConfig(AppConfig):
+    BASE_DIR = settings.BASE_DIR
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'DaveIsTheBest_base'
-    with open('./DaveIsTheBest_base/spam_filter_vectorizer.sav','rb') as sv:
+    with open(BASE_DIR / '/DaveIsTheBest_base/spam_filter_vectorizer.sav','rb') as sv:
         spam_vectorizer = load(sv)
-    with open('./DaveIsTheBest_base/spam_filter_model.sav','rb') as sf:
+    with open(BASE_DIR / '/DaveIsTheBest_base/spam_filter_model.sav','rb') as sf:
         spam_filter_model = load(sf)
