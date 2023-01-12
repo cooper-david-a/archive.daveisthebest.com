1d73dcdb85570641584e27ad57a484feefc1d6f0
Dave <cooper.david.a@protonmail.com>
2023-01-12 15:30:59 -0600

    update spam filter

--- file_sharing/models.py
+++ file_sharing/models.py
@@ -6,8 +6,11 @@ from django.conf import settings
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.user.username
+
 class SharedFile(models.Model):
     file_name = models.CharField(max_length=100)
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    file = models.FileField(upload_to='filesharing/')
+    file = models.FileField(upload_to='file_sharing/')
     
--- file_sharing/views.py
+++ file_sharing/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import ListView
+from .models import SharedFile, Profile
 
 # Create your views here.
 
 class FileSharingListView(ListView):
-    pass
+    model = SharedFile
+    template_name = 'file_sharing/file_sharing_list.html'
