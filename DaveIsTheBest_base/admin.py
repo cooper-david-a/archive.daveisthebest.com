2b64efcc6d64851f5cf146a687771af7730fbf57
Dave <cooper.david.a@protonmail.com>
2023-02-04 15:22:57 -0600

    Finish customizing user model

--- DaveIsTheBest_base/admin.py
+++ DaveIsTheBest_base/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
-from .models import Comment
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import Comment, User
 
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+     add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2", "email", "first_name", "last_name",),
+            },
+        ),
+    )
+
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ['id', 'ok_to_display', 'date_entered', 'commenter_name', 'comment_text', 'parent_comment',
